f36ea1b909c0444bb590d98a529180bbe10e67fd
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-10 15:28:21 +0100

    ex-2

--- ex_join.sql
+++ ex_join.sql
@@ -5,3 +5,11 @@ FROM `students`
 JOIN `degrees`
 ON `degrees`.`id` = `students`.`degree_id`
 WHERE `degrees`.`name` LIKE '%Economia';
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+SELECT `degrees`.`name` AS 'Nome_corso', `departments`.`name` AS 'Dipartimento'
+FROM `degrees`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
