f0d32aa11e5258a4d7227bb91003e3914f620a2c
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-10 15:53:02 +0100

    ex-4

--- ex_join.sql
+++ ex_join.sql
@@ -23,3 +23,14 @@ ON `degrees`.`id` = `course_teacher`.`course_id`
 JOIN `teachers`
 ON `teachers`.`id` = `course_teacher`.`teacher_id`
 WHERE `teachers`.`name` = 'Fulvio' AND `teachers`.`surname` = 'Amato';
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea 
+-- a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT `students`.`name`  AS 'Nome', `students`.`surname` AS 'Cognome', `degrees`.*, `departments`.`name`
+FROM `students`
+JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+JOIN `departments`
+ON`departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname` ASC;
