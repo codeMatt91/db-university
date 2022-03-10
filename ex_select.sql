d78bdc794edb0789834f0fc0077688a9ac835c9d
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-10 10:13:18 +0100

    Fixed ex-1bis

--- ex_select.sql
+++ ex_select.sql
@@ -67,8 +67,8 @@ WHERE `phone` IS NULL;
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
-SELECT COUNT(*) AS 'iscritti_per_anno',  `year`
-FROM `courses` 
+SELECT COUNT(*) AS 'iscritti_per_anno',  YEAR(`enrolment_date`) AS `year`
+FROM `students` 
 GROUP BY `year`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
